6b29700c4817c2580a1076b07261ad16f4327842
Nick Brandaleone <nbrand@mac.com>
2015-04-19 10:53:35 -0400

    Initial Commit

---
+++ PubNubTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PubNubTest
+//
+//  Created by Nick Brandaleone on 4/19/15.
+//  Copyright (c) 2015 Nick Brandaleone. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
